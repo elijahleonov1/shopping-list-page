bd0a8cefccf8b78fbc7f50acc4b4868f3182f662
elijah <ex@gmail.com>
2020-05-31 01:05:56 +0300

    _

---
+++ precache-manifest.74800fe1d650c6b13b319e756e3895eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5b516780f50944ba5aed20d3e9de29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d384d50e9dbfa332fb92",
+    "url": "/static/css/2.814df5ce.chunk.css"
+  },
+  {
+    "revision": "d384d50e9dbfa332fb92",
+    "url": "/static/js/2.7d6e36bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d6e36bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c1dbcd21ce1edc85a1",
+    "url": "/static/js/main.e13841b6.chunk.js"
+  },
+  {
+    "revision": "ab525d4571ff2e89fc73",
+    "url": "/static/js/runtime-main.031aa52d.js"
+  }
+]);
